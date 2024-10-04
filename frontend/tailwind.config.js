55eb9ebca23cdbad2ac1bf3da1bbefaee084c4ce
Sebastián García D. <sebastian.garcia@pucv.cl>
2024-10-04 12:05:32 -0300

    [frontend] - Configuraciones básicas de la aplicación
    - Se añaden configuraciones para prettier y tailwind
    - Se agregan metadatos en cabecera de index.html

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}',
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'hacker-green': {
+          DEFAULT: '#29A400',
+          soft: '#B6FF80'
+        }
+      }
+    }
   },
-  plugins: [],
-}
-
+  corePlugins: {
+    aspectRatio: false
+  },
+  plugins: [require('@tailwindcss/aspect-ratio')]
+};
